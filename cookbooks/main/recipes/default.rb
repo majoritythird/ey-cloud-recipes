6eadb3678e25bb11da7f8ef171ce890dc707efb9
Wes Gibbs <wes@majoritythird.com>
2011-02-07 17:31:39 -0500

    Imagemagick recipe

--- cookbooks/main/recipes/default.rb
+++ cookbooks/main/recipes/default.rb
@@ -16,6 +16,8 @@
 # uncomment to turn on thinking sphinx/ultra sphinx. Remember to edit cookbooks/sphinx/recipes/default.rb first!
 require_recipe "sphinx"
 
+require_recipe "imagemagick"
+
 #uncomment to turn on memcached
 # require_recipe "memcached"
 
