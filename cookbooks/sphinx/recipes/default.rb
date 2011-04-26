9948e647c6a8cc30d0edab01dd2aac41f456e408
Wes Gibbs <wes@majoritythird.com>
2011-04-26 11:48:15 -0400

    Move sphinx to utility instance; enable cron

--- cookbooks/sphinx/recipes/default.rb
+++ cookbooks/sphinx/recipes/default.rb
@@ -16,8 +16,8 @@ flavor = "thinking_sphinx"
 # deploy may fail because the initial database migration will not have
 # run by the time this executes on the utility instance. If that occurs
 # just deploy again and the recipe should succeed.
-utility_name = nil
-# utility_name = "sphinx"
+# utility_name = nil
+utility_name = "sphinx"
 
 # If you want to have scheduled reindexes in cron, enter the minute
 # interval here. This is passed directly to cron via /, so you should
@@ -25,7 +25,7 @@ utility_name = nil
 #
 # If you don't want scheduled reindexes, just leave this set to nil.
 # Setting it equal to 10 would run the cron job every 10 minutes.
-cron_interval = nil
+cron_interval = 5
 
 if utility_name
   if ['solo', 'app', 'app_master'].include?(node[:instance_role])
